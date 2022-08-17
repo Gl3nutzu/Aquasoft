239983d61fd45a99184bcd5300b3d62ef791c82d
sorinciubotaru <82326757+sorinciubotaru@users.noreply.github.com>
2022-08-17 13:25:17 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const mysql = require('mysql2');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// testing api
+app.get('/', (req, res) => {
+    res.json({ message: 'hello from api' })
+})
+
+// routers
+const router = require('./routes/projectRouter.js')
+app.use('/api/projects', router);
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
